a83a9f105fb17b2dde59698803405db87e1f6b18
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-17 19:40:44 +0300

    adding a filter and caching using useMemo

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,18 +1,22 @@
+import { useMemo } from "react"
 import styles from "./Home.module.css";
-import {cars} from "./cars.data.tsx"
-import CarItem from "./car-item/CarItem.tsx"
+import { cars } from "./cars.data.tsx";
+import CarItem from "./car-item/CarItem.tsx";
 const Home = () => {
+  const filteredCars = useMemo(
+    () => cars.filter((car) => car.price > 20000), []
+  )
+
   return (
     <>
       <h1>Cars catalog</h1>
-        <div className={styles.items}>
-            {cars.length ? cars.map(car => ( 
-                <CarItem key={car.id} car={car}/>
-            ))
-            : <p>There are no cars</p>
-        }
-            
-        </div>
+      <div className={styles.items}>
+        {filteredCars.length ? (
+          filteredCars.map((car) => <CarItem key={car.id} car={car} />)
+        ) : (
+          <p>There are no cars</p>
+        )}
+      </div>
     </>
   );
 };
