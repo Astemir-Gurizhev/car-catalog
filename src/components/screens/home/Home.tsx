e914feb312a237d0207b0f90edea66f656a8dc3f
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-17 19:15:40 +0300

    adding machine data and importing. outputting cars via map. adding a key for an item.

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,24 +1,28 @@
 import styles from "./Home.module.css";
+import {cars} from "./cars.data.tsx"
 
 const Home = () => {
   return (
     <>
       <h1>Cars catalog</h1>
-      <div>
-        <div className={styles.item}>
-          <div
-            className={styles.image}
-            style={{
-              backgroundImage: "url(/e63.jpg)",
-            }}
-          />
-          <div className={styles.info}>
-            <h2>Mercedes E63 AMG</h2>
-            <p>$30 000</p>
-            <button>Read more</button>
-          </div>
+        <div>
+            {cars.map(car => ( 
+                <div key={car.id} className={styles.item}>
+                    <div
+                        className={styles.image}
+                        style={{
+                        backgroundImage: `url(${car.image})`,
+                        }}
+                    />
+                    <div className={styles.info}>
+                        <h2>{car.name}</h2>
+                        <p>${car.price}</p>
+                        <button>Read more</button>
+                    </div>
+                </div>
+            ))}
+            
         </div>
-      </div>
     </>
   );
 };
