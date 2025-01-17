6fbe6829c0d1cf099b92df7f59343be1c6758301
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-17 15:33:18 +0300

    add  first post car, module css, added image

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,11 +1,25 @@
-import styles from "./Home.module.css"
+import styles from "./Home.module.css";
 
 const Home = () => {
-    return (
-        <>
-            <h1>Cars</h1>
-            
-        </>
-    )
-}
-export default Home
+  return (
+    <>
+      <h1>Cars catalog</h1>
+      <div>
+        <div className={styles.item}>
+          <div
+            className={styles.image}
+            style={{
+              backgroundImage: "url(/e63.jpg)",
+            }}
+          />
+          <div className={styles.info}>
+            <h2>Mercedes E63 AMG</h2>
+            <p>$30 000</p>
+            <button>Read more</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Home;
