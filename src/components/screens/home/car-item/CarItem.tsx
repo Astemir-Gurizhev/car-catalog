8c3198d2ad7add75b6674b70510d353ce0b01cad
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-19 13:22:47 +0300

    Separation of the Price logic by creating the Price component in the car-item

--- src/components/screens/home/car-item/CarItem.tsx
+++ src/components/screens/home/car-item/CarItem.tsx
@@ -1,5 +1,6 @@
 import styles from "../Home.module.css";
 import { Link } from "react-router-dom";
+import Price from "./Price";
 const CarItem = ({ car }) => {
   return (
     <div className={styles.item}>
@@ -11,13 +12,11 @@ const CarItem = ({ car }) => {
       />
       <div className={styles.info}>
         <h2>{car.name}</h2>
-        <p>
-          {new Intl.NumberFormat("ru-Ru", {
-            style: "currency",
-            currency: "USD",
-          }).format(car.price)}
-        </p>
-        <Link className='btn' to={`/car/${car.id}`}>Read more</Link>
+        <Price price={car.price}/>
+        <Link className="btn" to={`/car/${car.id}`}>
+          Read more
+        </Link>
+        
       </div>
     </div>
   );
---
+++ src/components/screens/home/car-item/Price.tsx
@@ -0,0 +1,11 @@
+const Price = ({price}) => {
+  return (
+    <p>
+      {new Intl.NumberFormat("ru-Ru", {
+        style: "currency",
+        currency: "USD",
+      }).format(price)}
+    </p>
+  );
+};
+export default Price;
